f570e9faa3a39d5133a6a34a95c305339cce4b1b
Alexander Nortung <alex_nortung@live.dk>
2022-01-23 15:41:17 +0100

    added papermc

--- default.nix
+++ default.nix
@@ -15,6 +15,15 @@
   overlays = import ./overlays; # nixpkgs overlays
 
   example-package = pkgs.callPackage ./pkgs/example-package { };
+  papermc-1_18_x = pkgs.callPackage ./pkgs/games/papermc/1.18.nix { };
+  papermc-1_17_x = pkgs.callPackage ./pkgs/games/papermc/1.17.nix { };
+  papermc-1_16_x = pkgs.callPackage ./pkgs/games/papermc/1.16.nix { };
+  papermc-1_15_x = pkgs.callPackage ./pkgs/games/papermc/1.15.nix { };
+  papermc-1_14_x = pkgs.callPackage ./pkgs/games/papermc/1.14.nix { };
+  papermc-1_13_x = pkgs.callPackage ./pkgs/games/papermc/1.13.nix { };
+  papermc-1_12_x = pkgs.callPackage ./pkgs/games/papermc/1.12.nix { };
+  papermc-1_11_x = pkgs.callPackage ./pkgs/games/papermc/1.11.nix { };
+  papermc-1_10_x = pkgs.callPackage ./pkgs/games/papermc/1.10.nix { };
   # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
   # ...
 }
